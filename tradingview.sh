ddad2820f56c6699fd15f4380ac4016bd2a726b8
mountaineerbr <jsonineto@gmail.com>
2021-01-25 09:24:30 -0300

    sync

--- tradingview.sh
+++ tradingview.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/bash
-
 #open some tradingview windows at specific screen positions
-
 #screen resolution at 1366x768, one display
 
 #sleep between new windows opening
@@ -12,36 +10,36 @@ SNAME="${0##*/}"
 #get window id
 getidf() {
 	ids=( $( xdotool search --name tradingview ) )
-	id="${ids[-1]}" ||
-		echo "$SNAME: cannot get window id" >&2
+	ID="${ids[-1]}" ||
+		echo "$SNAME: cannot get window ID" >&2
 }
 
 #demonise first google-chrome
 {
 google-chrome-stable --app=https://www.tradingview.com/chart/ISmYtO7U/
 getidf
-#first sleep may need to be increased to open chrome fully
-xdotool windowsize "$id" 680 700 
-xdotool windowmove "$id" 0 0
+xdotool windowsize "$ID" 680 700 
+xdotool windowmove "$ID" 0 0
 } &
+#first sleep may need to be increased to allow chrome opening fully
 sleep 6
 
 google-chrome-stable --app=https://www.tradingview.com/chart/vSIVmcxS/
 getidf
-xdotool windowsize "$id" 680 700 
-xdotool windowmove "$id" 680 0
+xdotool windowsize "$ID" 680 700 
+xdotool windowmove "$ID" 680 0
 sleep $SLEEP
 
 google-chrome-stable --app=https://www.tradingview.com/chart/h2YRqfnP/
 getidf
-xdotool windowsize "$id" 680 700
-xdotool windowmove "$id" 10 10
+xdotool windowsize "$ID" 680 700
+xdotool windowmove "$ID" 10 10
 sleep $SLEEP
 
 google-chrome-stable --app=https://www.tradingview.com/chart/nNx9sErC/
 getidf
-xdotool windowsize "$id" 680 700 
-xdotool windowmove "$id" 690 10
+xdotool windowsize "$ID" 680 700 
+xdotool windowmove "$ID" 690 10
 
 disown 
 echo "$SNAME: took $SECONDS seconds to execute" >&2
